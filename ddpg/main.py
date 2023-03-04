e3702b5eb279fa7596cc0501606ebe7022959302
Bryson Jones <bkjones97@gmail.com>
2023-03-03 18:46:54 -0800

    start laying out code skeleton

--- ddpg/main.py
+++ ddpg/main.py
@@ -10,3 +10,19 @@ if __name__ == "__main__":
     env = gym.make("") # TODO: pick a good, simple continuous action space
     action_space_size = env.action_space.n
     obs_space_size = env.observation_space.shape[0]
+
+    # initialize networks
+    # DQN
+    # policy
+
+    # init replay buffer
+
+    # create target networks
+    # TODO: is that just copying?
+
+    # loop()
+        # select action, and add zero mean gaussian noise to selected actions
+        # execute action
+        # get next_state, rewards, done signal
+        # store all data in replay buffer
+        
