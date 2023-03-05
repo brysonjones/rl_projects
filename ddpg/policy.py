50fcbc74ca57d82c141887ffaed06101a02a63e9
brysonjones <bkjones97@gmail.com>
2023-03-05 08:36:55 -0800

    add policy first iteration

--- ddpg/policy.py
+++ ddpg/policy.py
@@ -6,11 +6,26 @@ class PolicyNetwork(nn.Module):
     def __init__(self, obs_space_size, action_space_size,
                  num_hidden, num_layers, activation='ReLU'):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_activation_stack = nn.Sequential(
 
-        )
+        # TODO: define this somewhere else
+        activation_dict = {}
+        activation_dict['ReLU'] = nn.ReLU()
+        activation_dict['Tanh'] = nn.Tanh()
 
+        layer_list = []
+        for i_layer in range(num_layers):
+            if len(layer_list) == 0:
+                layer_list.append(nn.Linear(obs_space_size, num_hidden))
+                layer_list.append(activation_dict[activation]())
+            else:
+                layer_list.append(nn.Linear(num_hidden, num_hidden))
+                layer_list.append(activation_dict[activation]())
+
+        layer_list.append((nn.Linear(num_hidden, action_space_size)))
+
+        self.linear_activation_stack = nn.Sequential(nn.Flatten(), 
+                                                     *layer_list)
+                                                
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_activation_stack(x)
